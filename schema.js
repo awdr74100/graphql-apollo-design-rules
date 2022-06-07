d1e20ccab090f814217305f151e7cda5af5af5f8
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-08 01:57:08 +0800

    Update schema.js

--- schema.js
+++ schema.js
@@ -10,6 +10,33 @@ const typeDefs = gql`
     color: String!
     make: String!
   }
+
+  type ManualGroup {
+    id: ID!
+    name: String!
+    imageId: ID!
+    bodyHtml: String!
+    memberships: [GroupMembership!]!
+  }
+
+  type AutomaticGroup {
+    id: ID!
+    name: String!
+    imageId: ID!
+    bodyHtml: String!
+    memberships: [GroupMembership!]!
+    feature: [AutomaticGroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type AutomaticGroupFeatures {
+    column: String!
+  }
+
+  type GroupMembership {
+    groupId: ID!
+    carId: ID!
+  }
 `;
 
 module.exports = typeDefs;
