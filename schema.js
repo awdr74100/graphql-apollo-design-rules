ea11b193691be1f0e9b4cb113ed11d93b5f883aa
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-09 04:14:08 +0800

    Update schema.js

--- schema.js
+++ schema.js
@@ -6,13 +6,22 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    create
-    delete
-    update
-    publish
-    unpublish
-    addCars
-    removeCars
+    groupDelete(groupId: ID!)
+    groupPublish(groupId: ID!)
+    groupUnpublish(groupId: ID!)
+    groupAddCars(groupId: ID!, carId: ID!)
+    groupRemoveCars(groupId: ID!, carId: ID!)
+    groupCreate(
+      name: String!
+      image: ImageInput!
+      description: String!
+      featureSet: GroupFeatureFields
+    )
+    groupUpdate
+  }
+
+  input ImageInput {
+    url: String!
   }
 
   type Car {
